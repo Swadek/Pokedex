c6e09280d3bc5fcead7a1d28666192ff2ac25de3
Antoine JULIAN <antoine.julian@hotmail.com>
2023-04-03 10:45:54 +0200

    React08

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 
 function NavBar({ count, setCount, pokemonList }) {
-  const handleClickNext = () => {
-    setCount(count + 1);
-  };
-  const handleClickPrevious = () => {
-    setCount(count - 1);
-  };
-  /* J'ai créé une boucle plutôt que de faire disparaitre les boutons*/
-  count === -1
-    ? setCount(pokemonList.length - 1)
-    : count === pokemonList.length
-    ? setCount(0)
-    : count;
   return (
     <div>
-      <button onClick={handleClickPrevious}>Previous</button>
-      <button onClick={handleClickNext}>Next</button>
+      {pokemonList.map((el, index) => (
+        <button key={index} onClick={() => setCount((count = index))}>
+          {el.name}
+        </button>
+      ))}
     </div>
   );
 }
